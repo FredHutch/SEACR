f59a7dc72f1ba250f6154dc3f5bc7a5126beeece
mpmeers <mpmeers@gmail.com>
2019-02-11 11:42:04 -0800

    Update SEARCh_1.0.sh

--- SEARCh_1.0.sh
+++ SEARCh_1.0.sh
@@ -16,14 +16,14 @@ then
 
 	Example:
 
-	bash SEARCh_1.0.sh MPM091.bedgraph MPM099.bedgraph norm AUC
-	Calls enriched regions in MPM091 using normalized IgG control track from MPM099
+	bash SEARCh_1.0.sh target.bedgraph IgG.bedgraph norm AUC
+	Calls enriched regions in target data using normalized IgG control track with AUC threshold
 	
-	bash SEARCh_1.0.sh MPM091.bedgraph MPM099.bedgraph non union
-	Calls enriched regions in MPM091 using non-normalized IgG control track from MPM099 
+	bash SEARCh_1.0.sh target.bedgraph MPM099.bedgraph non union
+	Calls enriched regions in target data using non-normalized IgG control track with AUC and max signal thresholds
 
-	bash SEARCh_1.0.sh MPM091.bedgraph 0.01 non AUC
-	Calls enriched regions in MPM091 by selecting the top 1% of regions by area under the curve (AUC)
+	bash SEARCh_1.0.sh target.bedgraph 0.01 non AUC
+	Calls enriched regions in target data by selecting the top 1% of regions by area under the curve (AUC)
 
 	"
 	exit 1
