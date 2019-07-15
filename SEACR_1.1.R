28b2c74395037fd7c22c41e9bcac1353ddef2c74
mpmeers <mpmeers@gmail.com>
2019-07-15 11:16:12 -0700

    Update SEACR_1.1.R
    Fixed bug that omitted z or b when x0==z2 or a0==b2

--- SEACR_1.1.R
+++ SEACR_1.1.R
@@ -68,8 +68,12 @@ if(is.na(numtest)){ ## If 2nd field is a bedgraph, calculate empirical threshold
 	x0<-x[which(na.omit(pctremain(x[pctremain(x) < 1])) == max(na.omit(pctremain(x[pctremain(x) < 1]))))]  ## New for SEACR_1.1
 	z<-x[x <= x0[1]]	## New for SEACR_1.1
 	z2<-z[abs(((pctremain(x0)+min(pctremain(z)))/2)-pctremain(z))==min(abs(((pctremain(x0)+min(pctremain(z)))/2)-pctremain(z)))] ## New for SEACR_1.1
-	z<-z[z > z2[1]] ## New for SEACR_1.1
-	z0<-z[abs(z-(max(z)-((1/2)*(max(z)-min(z)))))==min(abs(z-(max(z)-((1/2)*(max(z)-min(z))))))] ## New for SEACR_1.1
+	if(x0!=z2){  ## Added 7/15/19 to avoid omitting z when x0==z2
+		z<-z[z > z2[1]] ## New for SEACR_1.1
+		z0<-z[abs(z-(max(z)-((1/2)*(max(z)-min(z)))))==min(abs(z-(max(z)-((1/2)*(max(z)-min(z))))))] ## New for SEACR_1.1
+	}else{  ## Added 7/15/19 to avoid omitting z when x0==z2
+		z0<-x0  ## Added 7/15/19 to avoid omitting z when x0==z2
+	}  ## Added 7/15/19 to avoid omitting z when x0==z2
 	
 	## The following code segment was added to avoid spurious high thresholding when the peak of a lower threshold is within 95% of the peak of the maximum threshold
 	
@@ -87,8 +91,12 @@ if(is.na(numtest)){ ## If 2nd field is a bedgraph, calculate empirical threshold
 	a0<-a[which(na.omit(pctremain(a[pctremain(a) < 1])) == max(na.omit(pctremain(a[pctremain(a) <  1]))))]
 	b<-a[a <= a0[1]]
 	b2<-b[abs(((pctremain(a0)+min(pctremain(b)))/2)-pctremain(b))==min(abs(((pctremain(a0)+min(pctremain(b)))/2)-pctremain(b)))]
-	b<-b[b > b2[1]]
-	b0<-b[abs(b-(max(b)-((1/2)*(max(b)-min(b)))))==min(abs(b-(max(b)-((1/2)*(max(b)-min(b))))))]
+	if(a0!=b2){  ## Added 7/15/19 to avoid omitting b when a0==b2
+		b<-b[b > b2[1]]
+		b0<-b[abs(b-(max(b)-((1/2)*(max(b)-min(b)))))==min(abs(b-(max(b)-((1/2)*(max(b)-min(b))))))]
+	}else{  ## Added 7/15/19 to avoid omitting b when a0==b2
+		b0<-a0  ## Added 7/15/19 to avoid omitting b when a0==b2
+	}  ## Added 7/15/19 to avoid omitting b when a0==b2
 	if(max(na.omit(pctremain(a[pctremain(a) < 1])))/max(na.omit(pctremain(x[pctremain(x) < 1]))) > 0.95){
 		x0<-a0
 		z0<-b0
