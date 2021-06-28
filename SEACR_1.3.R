4609500359cff9491146db38b2b7a2c2f50784da
mpmeers <mpmeers@gmail.com>
2021-06-27 17:57:51 -0700

    Update SEACR_1.3.R
    Add loop at line 102 to determine whether frame$diff contains nonzero entries. If nonzero entries don't exist, skip the code block from line 104-125.

--- SEACR_1.3.R
+++ SEACR_1.3.R
@@ -100,27 +100,29 @@ if(is.na(numtest)){ ## If 2nd field is a bedgraph, calculate empirical threshold
 	
 	frame<-data.frame(thresh=x[1:(length(x)-1)], pct=pctremain(x[1:(length(x)-1)]), diff=abs(diff(pctremain(x))))
 	frame<-na.omit(frame)
-	i<-2
-	output<-0
-	while(output==0){
-		test3<-as.numeric(paste(c(0,".",rep(9,i)),sep="",collapse=""))
-		output<-as.numeric(quantile(frame$diff, test3))
-#		print(output)
-		i<-i+1
-	}
-	a<-frame$thresh[frame$diff != 0 & frame$diff < quantile(frame$diff, test3)]
-	a0<-a[which(na.omit(pctremain(a[pctremain(a) < 1])) == max(na.omit(pctremain(a[pctremain(a) <  1]))))]
-	b<-a[a <= a0[1]]
-	b2<-b[abs(((pctremain(a0)+min(pctremain(b)))/2)-pctremain(b))==min(abs(((pctremain(a0)+min(pctremain(b)))/2)-pctremain(b)))]
-	if(a0[1]!=b2[1]){  ## Added 7/15/19 to avoid omitting b when a0==b2
-		b<-b[b > b2[1]]
-		b0<-b[abs(b-(max(b)-((1/2)*(max(b)-min(b)))))==min(abs(b-(max(b)-((1/2)*(max(b)-min(b))))))]
-	}else{  ## Added 7/15/19 to avoid omitting b when a0==b2
-		b0<-a0  ## Added 7/15/19 to avoid omitting b when a0==b2
-	}  ## Added 7/15/19 to avoid omitting b when a0==b2
-	if(max(na.omit(pctremain(a[pctremain(a) < 1])))/max(na.omit(pctremain(x[pctremain(x) < 1]))) > 0.95){
-		x0<-a0
-		z0<-b0
+	if(max(frame$diff > 0){
+		i<-2
+		output<-0
+		while(output==0){
+			test3<-as.numeric(paste(c(0,".",rep(9,i)),sep="",collapse=""))
+			output<-as.numeric(quantile(frame$diff, test3))
+	#		print(output)
+			i<-i+1
+		}
+		a<-frame$thresh[frame$diff != 0 & frame$diff < quantile(frame$diff, test3)]
+		a0<-a[which(na.omit(pctremain(a[pctremain(a) < 1])) == max(na.omit(pctremain(a[pctremain(a) <  1]))))]
+		b<-a[a <= a0[1]]
+		b2<-b[abs(((pctremain(a0)+min(pctremain(b)))/2)-pctremain(b))==min(abs(((pctremain(a0)+min(pctremain(b)))/2)-pctremain(b)))]
+		if(a0[1]!=b2[1]){  ## Added 7/15/19 to avoid omitting b when a0==b2
+			b<-b[b > b2[1]]
+			b0<-b[abs(b-(max(b)-((1/2)*(max(b)-min(b)))))==min(abs(b-(max(b)-((1/2)*(max(b)-min(b))))))]
+		}else{  ## Added 7/15/19 to avoid omitting b when a0==b2
+			b0<-a0  ## Added 7/15/19 to avoid omitting b when a0==b2
+		}  ## Added 7/15/19 to avoid omitting b when a0==b2
+		if(max(na.omit(pctremain(a[pctremain(a) < 1])))/max(na.omit(pctremain(x[pctremain(x) < 1]))) > 0.95){
+			x0<-a0
+			z0<-b0
+		}
 	}
 	both2<-c(expmax,ctrlmax)
 	d<-sort(unique(both2))
